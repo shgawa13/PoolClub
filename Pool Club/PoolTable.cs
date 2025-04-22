e661a41ad0cd006eb9d48fcc5ea1fe0d698712bf
Captain Black <shgawa.cp@gmail.com>
2025-04-22 21:32:37 +0300

    Starting PoolClub Project

---
+++ Pool Club/PoolTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pool_Club
+{
+  public partial class PoolTable : UserControl
+  {
+    
+
+    public PoolTable()
+    {
+      InitializeComponent();
+    }
+
+    protected override void OnPaint(PaintEventArgs pe)
+    {
+      base.OnPaint(pe);
+    }
+
+
+    // Table Event
+    public class PoolTableEvent : EventArgs
+    {
+      public string TimeText { get; }
+      public int TimeInSeconds { get; }
+      public float TotolFees { get; }
+      public float RatePerHour { get; } 
+    }
+
+
+  }
+}
