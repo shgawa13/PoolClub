b5677be1367d0a883f2cadab8f28a1949f3e092d
Captain Black <shgawa.cp@gmail.com>
2025-05-10 09:40:19 +0300

    adding save and load functions

--- Pool Club/Players/frmAddUpdatePlayer.Designer.cs
+++ Pool Club/Players/frmAddUpdatePlayer.Designer.cs
@@ -34,29 +34,27 @@
       this.lblPlayersNumber = new System.Windows.Forms.Label();
       this.label2 = new System.Windows.Forms.Label();
       this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.cmbMemberShips = new System.Windows.Forms.ComboBox();
+      this.txbLastName = new System.Windows.Forms.TextBox();
+      this.txbPhone = new System.Windows.Forms.TextBox();
+      this.txbFirstName = new System.Windows.Forms.TextBox();
+      this.lblPlayerID = new System.Windows.Forms.Label();
       this.label1 = new System.Windows.Forms.Label();
-      this.label3 = new System.Windows.Forms.Label();
       this.label4 = new System.Windows.Forms.Label();
       this.label6 = new System.Windows.Forms.Label();
-      this.textBox1 = new System.Windows.Forms.TextBox();
-      this.textBox2 = new System.Windows.Forms.TextBox();
-      this.textBox3 = new System.Windows.Forms.TextBox();
-      this.comboBox1 = new System.Windows.Forms.ComboBox();
-      this.ctrlExpertBtn1 = new Pool_Club.Controls.ctrlExpertBtn();
+      this.btnSave = new Pool_Club.Controls.ctrlExpertBtn();
       this.btnCancel = new Pool_Club.Controls.ctrlExpertBtn();
-      this.chbIsActive = new Syncfusion.Windows.Forms.Tools.CheckBoxAdv();
       ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
       this.panelDown.SuspendLayout();
       this.groupBox1.SuspendLayout();
-      ((System.ComponentModel.ISupportInitialize)(this.chbIsActive)).BeginInit();
       this.SuspendLayout();
       // 
       // lblTitle
       // 
       this.lblTitle.AutoSize = true;
       this.lblTitle.Font = new System.Drawing.Font("Ebrima", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.lblTitle.ForeColor = System.Drawing.Color.GhostWhite;
-      this.lblTitle.Location = new System.Drawing.Point(314, 130);
+      this.lblTitle.ForeColor = System.Drawing.Color.Black;
+      this.lblTitle.Location = new System.Drawing.Point(294, 128);
       this.lblTitle.Name = "lblTitle";
       this.lblTitle.Size = new System.Drawing.Size(94, 25);
       this.lblTitle.TabIndex = 0;
@@ -65,7 +63,7 @@
       // pictureBox1
       // 
       this.pictureBox1.Image = global::Pool_Club.Properties.Resources.Players;
-      this.pictureBox1.Location = new System.Drawing.Point(274, 12);
+      this.pictureBox1.Location = new System.Drawing.Point(253, 12);
       this.pictureBox1.Name = "pictureBox1";
       this.pictureBox1.Size = new System.Drawing.Size(172, 109);
       this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -74,13 +72,13 @@
       // 
       // panelDown
       // 
-      this.panelDown.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(35)))), ((int)(((byte)(48)))));
-      this.panelDown.Controls.Add(this.groupBox1);
+      this.panelDown.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(99)))), ((int)(((byte)(160)))));
       this.panelDown.Controls.Add(this.pictureBox1);
       this.panelDown.Controls.Add(this.lblTitle);
+      this.panelDown.Dock = System.Windows.Forms.DockStyle.Top;
       this.panelDown.Location = new System.Drawing.Point(0, 0);
       this.panelDown.Name = "panelDown";
-      this.panelDown.Size = new System.Drawing.Size(675, 400);
+      this.panelDown.Size = new System.Drawing.Size(674, 160);
       this.panelDown.TabIndex = 4;
       // 
       // lblPlayersNumber
@@ -88,8 +86,8 @@
       this.lblPlayersNumber.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
       this.lblPlayersNumber.AutoSize = true;
       this.lblPlayersNumber.Font = new System.Drawing.Font("Ebrima", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.lblPlayersNumber.ForeColor = System.Drawing.Color.GhostWhite;
-      this.lblPlayersNumber.Location = new System.Drawing.Point(28, 134);
+      this.lblPlayersNumber.ForeColor = System.Drawing.Color.Black;
+      this.lblPlayersNumber.Location = new System.Drawing.Point(26, 144);
       this.lblPlayersNumber.Name = "lblPlayersNumber";
       this.lblPlayersNumber.Size = new System.Drawing.Size(114, 25);
       this.lblPlayersNumber.TabIndex = 1;
@@ -100,8 +98,8 @@
       this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
       this.label2.AutoSize = true;
       this.label2.Font = new System.Drawing.Font("Ebrima", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.label2.ForeColor = System.Drawing.Color.GhostWhite;
-      this.label2.Location = new System.Drawing.Point(28, 85);
+      this.label2.ForeColor = System.Drawing.Color.Black;
+      this.label2.Location = new System.Drawing.Point(23, 95);
       this.label2.Name = "label2";
       this.label2.Size = new System.Drawing.Size(117, 25);
       this.label2.TabIndex = 0;
@@ -109,57 +107,88 @@
       // 
       // groupBox1
       // 
-      this.groupBox1.Controls.Add(this.chbIsActive);
-      this.groupBox1.Controls.Add(this.comboBox1);
-      this.groupBox1.Controls.Add(this.textBox3);
-      this.groupBox1.Controls.Add(this.textBox2);
-      this.groupBox1.Controls.Add(this.textBox1);
-      this.groupBox1.Controls.Add(this.label3);
+      this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+      this.groupBox1.Controls.Add(this.cmbMemberShips);
+      this.groupBox1.Controls.Add(this.txbLastName);
+      this.groupBox1.Controls.Add(this.txbPhone);
+      this.groupBox1.Controls.Add(this.txbFirstName);
+      this.groupBox1.Controls.Add(this.lblPlayerID);
       this.groupBox1.Controls.Add(this.label1);
       this.groupBox1.Controls.Add(this.label4);
       this.groupBox1.Controls.Add(this.label6);
       this.groupBox1.Controls.Add(this.label2);
       this.groupBox1.Controls.Add(this.lblPlayersNumber);
+      this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
       this.groupBox1.Font = new System.Drawing.Font("Ebrima", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.groupBox1.ForeColor = System.Drawing.Color.Gainsboro;
-      this.groupBox1.Location = new System.Drawing.Point(12, 158);
+      this.groupBox1.ForeColor = System.Drawing.Color.Black;
+      this.groupBox1.Location = new System.Drawing.Point(0, 160);
       this.groupBox1.Name = "groupBox1";
-      this.groupBox1.Size = new System.Drawing.Size(652, 232);
+      this.groupBox1.Size = new System.Drawing.Size(674, 242);
       this.groupBox1.TabIndex = 2;
       this.groupBox1.TabStop = false;
       this.groupBox1.Text = "Player info";
+      // 
+      // cmbMemberShips
+      // 
+      this.cmbMemberShips.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.cmbMemberShips.FormattingEnabled = true;
+      this.cmbMemberShips.Location = new System.Drawing.Point(388, 125);
+      this.cmbMemberShips.Name = "cmbMemberShips";
+      this.cmbMemberShips.Size = new System.Drawing.Size(219, 28);
+      this.cmbMemberShips.TabIndex = 4;
+      // 
+      // txbLastName
+      // 
+      this.txbLastName.Location = new System.Drawing.Point(148, 143);
+      this.txbLastName.Name = "txbLastName";
+      this.txbLastName.Size = new System.Drawing.Size(169, 27);
+      this.txbLastName.TabIndex = 2;
+      // 
+      // txbPhone
+      // 
+      this.txbPhone.Location = new System.Drawing.Point(148, 188);
+      this.txbPhone.Name = "txbPhone";
+      this.txbPhone.Size = new System.Drawing.Size(169, 27);
+      this.txbPhone.TabIndex = 3;
+      // 
+      // txbFirstName
+      // 
+      this.txbFirstName.Location = new System.Drawing.Point(148, 94);
+      this.txbFirstName.Name = "txbFirstName";
+      this.txbFirstName.Size = new System.Drawing.Size(169, 27);
+      this.txbFirstName.TabIndex = 1;
+      // 
+      // lblPlayerID
+      // 
+      this.lblPlayerID.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.lblPlayerID.AutoSize = true;
+      this.lblPlayerID.Font = new System.Drawing.Font("Ebrima", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.lblPlayerID.ForeColor = System.Drawing.Color.Black;
+      this.lblPlayerID.Location = new System.Drawing.Point(143, 53);
+      this.lblPlayerID.Name = "lblPlayerID";
+      this.lblPlayerID.Size = new System.Drawing.Size(36, 25);
+      this.lblPlayerID.TabIndex = 3;
+      this.lblPlayerID.Text = "???";
       // 
       // label1
       // 
       this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
       this.label1.AutoSize = true;
       this.label1.Font = new System.Drawing.Font("Ebrima", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.label1.ForeColor = System.Drawing.Color.GhostWhite;
-      this.label1.Location = new System.Drawing.Point(43, 43);
+      this.label1.ForeColor = System.Drawing.Color.Black;
+      this.label1.Location = new System.Drawing.Point(43, 53);
       this.label1.Name = "label1";
       this.label1.Size = new System.Drawing.Size(97, 25);
       this.label1.TabIndex = 2;
       this.label1.Text = "PlayerID :";
-      // 
-      // label3
-      // 
-      this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-      this.label3.AutoSize = true;
-      this.label3.Font = new System.Drawing.Font("Ebrima", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.label3.ForeColor = System.Drawing.Color.GhostWhite;
-      this.label3.Location = new System.Drawing.Point(143, 45);
-      this.label3.Name = "label3";
-      this.label3.Size = new System.Drawing.Size(36, 25);
-      this.label3.TabIndex = 3;
-      this.label3.Text = "???";
       // 
       // label4
       // 
       this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
       this.label4.AutoSize = true;
       this.label4.Font = new System.Drawing.Font("Ebrima", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.label4.ForeColor = System.Drawing.Color.GhostWhite;
-      this.label4.Location = new System.Drawing.Point(403, 85);
+      this.label4.ForeColor = System.Drawing.Color.Black;
+      this.label4.Location = new System.Drawing.Point(403, 95);
       this.label4.Name = "label4";
       this.label4.Size = new System.Drawing.Size(189, 25);
       this.label4.TabIndex = 6;
@@ -170,68 +199,41 @@
       this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
       this.label6.AutoSize = true;
       this.label6.Font = new System.Drawing.Font("Ebrima", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.label6.ForeColor = System.Drawing.Color.GhostWhite;
-      this.label6.Location = new System.Drawing.Point(73, 179);
+      this.label6.ForeColor = System.Drawing.Color.Black;
+      this.label6.Location = new System.Drawing.Point(61, 189);
       this.label6.Name = "label6";
-      this.label6.Size = new System.Drawing.Size(69, 25);
+      this.label6.Size = new System.Drawing.Size(79, 25);
       this.label6.TabIndex = 4;
-      this.label6.Text = "Email :";
+      this.label6.Text = "Phone :";
       // 
-      // textBox1
+      // btnSave
       // 
-      this.textBox1.Location = new System.Drawing.Point(148, 86);
-      this.textBox1.Name = "textBox1";
-      this.textBox1.Size = new System.Drawing.Size(169, 27);
-      this.textBox1.TabIndex = 8;
-      // 
-      // textBox2
-      // 
-      this.textBox2.Location = new System.Drawing.Point(148, 183);
-      this.textBox2.Name = "textBox2";
-      this.textBox2.Size = new System.Drawing.Size(169, 27);
-      this.textBox2.TabIndex = 9;
-      // 
-      // textBox3
-      // 
-      this.textBox3.Location = new System.Drawing.Point(148, 135);
-      this.textBox3.Name = "textBox3";
-      this.textBox3.Size = new System.Drawing.Size(169, 27);
-      this.textBox3.TabIndex = 10;
-      // 
-      // comboBox1
-      // 
-      this.comboBox1.FormattingEnabled = true;
-      this.comboBox1.Location = new System.Drawing.Point(388, 125);
-      this.comboBox1.Name = "comboBox1";
-      this.comboBox1.Size = new System.Drawing.Size(219, 28);
-      this.comboBox1.TabIndex = 11;
-      // 
-      // ctrlExpertBtn1
-      // 
-      this.ctrlExpertBtn1.BackColor = System.Drawing.Color.MediumSlateBlue;
-      this.ctrlExpertBtn1.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
-      this.ctrlExpertBtn1.BorderColor = System.Drawing.Color.PaleVioletRed;
-      this.ctrlExpertBtn1.BorderRadius = 10;
-      this.ctrlExpertBtn1.BorderSize = 0;
-      this.ctrlExpertBtn1.FlatAppearance.BorderSize = 0;
-      this.ctrlExpertBtn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-      this.ctrlExpertBtn1.Font = new System.Drawing.Font("Ebrima", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.ctrlExpertBtn1.ForeColor = System.Drawing.Color.White;
-      this.ctrlExpertBtn1.Location = new System.Drawing.Point(384, 406);
-      this.ctrlExpertBtn1.Name = "ctrlExpertBtn1";
-      this.ctrlExpertBtn1.Size = new System.Drawing.Size(132, 45);
-      this.ctrlExpertBtn1.TabIndex = 5;
-      this.ctrlExpertBtn1.Text = "Save";
-      this.ctrlExpertBtn1.TextColor = System.Drawing.Color.White;
-      this.ctrlExpertBtn1.UseVisualStyleBackColor = false;
+      this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(99)))), ((int)(((byte)(160)))));
+      this.btnSave.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(99)))), ((int)(((byte)(160)))));
+      this.btnSave.BorderColor = System.Drawing.Color.PaleVioletRed;
+      this.btnSave.BorderRadius = 6;
+      this.btnSave.BorderSize = 0;
+      this.btnSave.FlatAppearance.BorderSize = 0;
+      this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.btnSave.Font = new System.Drawing.Font("Ebrima", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.btnSave.ForeColor = System.Drawing.Color.White;
+      this.btnSave.Location = new System.Drawing.Point(384, 406);
+      this.btnSave.Name = "btnSave";
+      this.btnSave.Size = new System.Drawing.Size(132, 45);
+      this.btnSave.TabIndex = 6;
+      this.btnSave.Text = "Save";
+      this.btnSave.TextColor = System.Drawing.Color.White;
+      this.btnSave.UseVisualStyleBackColor = false;
+      this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
       // 
       // btnCancel
       // 
-      this.btnCancel.BackColor = System.Drawing.Color.DimGray;
-      this.btnCancel.BackgroundColor = System.Drawing.Color.DimGray;
+      this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(22)))), ((int)(((byte)(28)))));
+      this.btnCancel.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(22)))), ((int)(((byte)(28)))));
       this.btnCancel.BorderColor = System.Drawing.Color.PaleVioletRed;
-      this.btnCancel.BorderRadius = 10;
+      this.btnCancel.BorderRadius = 6;
       this.btnCancel.BorderSize = 0;
+      this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
       this.btnCancel.FlatAppearance.BorderSize = 0;
       this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
       this.btnCancel.Font = new System.Drawing.Font("Ebrima", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -239,46 +241,33 @@
       this.btnCancel.Location = new System.Drawing.Point(532, 406);
       this.btnCancel.Name = "btnCancel";
       this.btnCancel.Size = new System.Drawing.Size(132, 45);
-      this.btnCancel.TabIndex = 6;
+      this.btnCancel.TabIndex = 7;
       this.btnCancel.Text = "Cancel";
       this.btnCancel.TextColor = System.Drawing.Color.White;
       this.btnCancel.UseVisualStyleBackColor = false;
-      // 
-      // chbIsActive
-      // 
-      this.chbIsActive.AccessibilityEnabled = true;
-      this.chbIsActive.BeforeTouchSize = new System.Drawing.Size(173, 24);
-      this.chbIsActive.Checked = true;
-      this.chbIsActive.CheckState = System.Windows.Forms.CheckState.Checked;
-      this.chbIsActive.Font = new System.Drawing.Font("Ebrima", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.chbIsActive.ForeColor = System.Drawing.SystemColors.InactiveBorder;
-      this.chbIsActive.ImageCheckBoxSize = new System.Drawing.Size(20, 20);
-      this.chbIsActive.Location = new System.Drawing.Point(388, 179);
-      this.chbIsActive.Name = "chbIsActive";
-      this.chbIsActive.Size = new System.Drawing.Size(173, 24);
-      this.chbIsActive.Style = Syncfusion.Windows.Forms.Tools.CheckBoxAdvStyle.Metro;
-      this.chbIsActive.TabIndex = 12;
-      this.chbIsActive.Text = "Is Active";
-      this.chbIsActive.ThemeName = "Metro";
+      this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
       // 
       // frmAddUpdatePlayer
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.BackColor = System.Drawing.SystemColors.Control;
+      this.BackColor = System.Drawing.SystemColors.ControlLightLight;
       this.ClientSize = new System.Drawing.Size(674, 458);
+      this.Controls.Add(this.groupBox1);
       this.Controls.Add(this.btnCancel);
-      this.Controls.Add(this.ctrlExpertBtn1);
+      this.Controls.Add(this.btnSave);
       this.Controls.Add(this.panelDown);
       this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
       this.Name = "frmAddUpdatePlayer";
       this.Text = "Add New Player";
+      this.Load += new System.EventHandler(this.frmAddUpdatePlayer_Load);
       ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
       this.panelDown.ResumeLayout(false);
       this.panelDown.PerformLayout();
       this.groupBox1.ResumeLayout(false);
       this.groupBox1.PerformLayout();
-      ((System.ComponentModel.ISupportInitialize)(this.chbIsActive)).EndInit();
       this.ResumeLayout(false);
 
     }
@@ -294,13 +283,12 @@
     private System.Windows.Forms.Label label4;
     private System.Windows.Forms.Label label6;
     private System.Windows.Forms.Label label1;
-    private System.Windows.Forms.Label label3;
-    private System.Windows.Forms.TextBox textBox3;
-    private System.Windows.Forms.TextBox textBox2;
-    private System.Windows.Forms.TextBox textBox1;
-    private System.Windows.Forms.ComboBox comboBox1;
-    private Controls.ctrlExpertBtn ctrlExpertBtn1;
+    private System.Windows.Forms.Label lblPlayerID;
+    private System.Windows.Forms.TextBox txbLastName;
+    private System.Windows.Forms.TextBox txbPhone;
+    private System.Windows.Forms.TextBox txbFirstName;
+    private System.Windows.Forms.ComboBox cmbMemberShips;
+    private Controls.ctrlExpertBtn btnSave;
     private Controls.ctrlExpertBtn btnCancel;
-    private Syncfusion.Windows.Forms.Tools.CheckBoxAdv chbIsActive;
   }
 }
--- Pool Club/Players/frmAddUpdatePlayer.cs
+++ Pool Club/Players/frmAddUpdatePlayer.cs
@@ -7,14 +7,103 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BusinessLayer;
 
 namespace Pool_Club.Players
 {
   public partial class frmAddUpdatePlayer : Form
   {
+    public enum enMode { AddNew=0, Update=1 }
+    private enMode _Mode = enMode.AddNew;
+
+    private int _PlayerID = -1;
+    private clsPlayer _Player;
+    private clsPerson _Person;
+
     public frmAddUpdatePlayer()
     {
       InitializeComponent();
+      _Mode = enMode.AddNew;
+     
+    }
+
+    public frmAddUpdatePlayer(int ID)
+    {
+      InitializeComponent();
+      _PlayerID = ID;
+
+      _Mode = enMode.Update;
+    }
+
+
+    // this function will rest the form to Defualt.
+    private void _ResetDefualtValues()
+    {
+      if (_Mode == enMode.AddNew) 
+      {
+        this.Text = "Add New";
+        lblTitle.Text = "Add New";
+        lblPlayerID.Text = "???";
+      }
+
+        txbFirstName.Text = string.Empty;
+      txbLastName.Text = string.Empty;
+      txbPhone.Text = string.Empty;
+    }
+
+    private void _LoadData()
+    {
+      _Player = clsPlayer.Find(_PlayerID);
+
+      // if null we will return.
+      if (_Player == null)
+      {
+        MessageBox.Show($"Error: couldn't find PlayerID: {_PlayerID}.",
+          "Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+        this.Close();
+        return;
+      }
+
+      // if not null we will fill the form with data.
+      lblPlayerID.Text = _Player.PlayerID.ToString();
+      txbFirstName.Text = _Player.PlayerInfo.FirstName;
+      txbLastName.Text = _Player.PlayerInfo.LastName;
+      txbPhone.Text = _Player.PlayerInfo.PhoneNumber;
+    }
+
+    private void frmAddUpdatePlayer_Load(object sender, EventArgs e)
+    {
+      if (_Mode == enMode.Update)
+        _LoadData();
+    }
+
+    // Now we Fill both Person and Player Obj.
+    
+    private void btnCancel_Click(object sender, EventArgs e)
+    {
+      this.Close();
+    }
+
+    private void btnSave_Click(object sender, EventArgs e)
+    {
+      _Person.FirstName = txbFirstName.Text.Trim();
+      _Person.LastName = txbLastName.Text.Trim();
+      _Person.PhoneNumber = txbPhone.Text.Trim();
+
+      if (_Person.Save())
+      {
+        // now we create player
+        _Player.PersonID = _Person.PersonID;
+        if (_Player.Save())
+        {
+          lblPlayerID.Text = _Player.PlayerID.ToString();
+          MessageBox.Show("Data Has been Save Successfully");
+        }
+        else
+        {
+          MessageBox.Show("Error: Something went worng couldn't Save Player.");
+        }
+      }
     }
   }
 }
